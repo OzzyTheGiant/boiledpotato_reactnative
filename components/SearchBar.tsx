30550cd992ccac9dbb03bb356e1188754c37e6fc
Oziel Perez <OZLperez11@gmail.com>
2020-01-04 20:00:23 -0600

    Added event handlers to text input and button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { View, TextInput, TouchableOpacity } from "react-native";
+import PropTypes from "prop-types";
+import { View, TextInput } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler"
 import SearchIcon from "assets/icons/search.svg";
 import mainStyles from "styles/Main";
 import componentStyles from "styles/Header";
 import { Dimens } from "styles/Main";
 
-export default function SearchBar() {
+export default function SearchBar({ onInput, onSubmit } : any) {
+	const { input, buttonIcon } = componentStyles;
+
+	function onChangeHandler(fieldText: string) {
+		onInput(fieldText);
+	}
+
 	return (
-		<View style={[mainStyles.component, componentStyles.header]}>
-			<TextInput style={componentStyles.input} placeholder="By ingredients"/>
-			<TouchableOpacity style={componentStyles.buttonIcon}>
+		<View style={ [mainStyles.component, componentStyles.header] }>
+			<TextInput style={input} placeholder="By ingredients" onChangeText={onChangeHandler}/>
+			<TouchableOpacity style={buttonIcon} onPress={onSubmit}>
 				<SearchIcon width={Dimens.buttonSizeMain} height={Dimens.buttonSizeMain}/>
 			</TouchableOpacity>
 		</View>
 	);
+}
+
+SearchBar.propTypes = {
+	keywords: PropTypes.string.isRequired,
+	onInput: PropTypes.func.isRequired,
+	onSubmit: PropTypes.func.isRequired
 }
