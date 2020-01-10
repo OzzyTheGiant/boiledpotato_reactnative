a5f3ca7555402d6799826a371a9d33fc7825b1a7
Oziel Perez <OZLperez11@gmail.com>
2020-01-10 00:16:58 -0600

    SearchBar displays error message if field is blank

--- components/MainScreen.tsx
+++ components/MainScreen.tsx
@@ -1,32 +1,56 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, StyleSheet } from "react-native";
 import { connect } from "react-redux";
+import EasyToast from "react-native-easy-toast";
 import Constants from "expo-constants";
+import * as actionCreator from "actions/actions";
 import Header from "components/Header";
 import SearchBar from "components/SearchBar";
 import CuisineFilter from "components/CuisineFilter";
-import * as actionCreator from "actions/actions";
+import { Colors } from "styles/Main";
 
 /** Main Screen component that displays search bar and cuisine filter buttons */
 function MainScreen({ query, editKeywords, startSearch, toggleCuisine } : any) {
+    const toast : React.MutableRefObject<EasyToast | null> = useRef(null)
+    const Toast : any = EasyToast
+
+    // check that search field is not empty, and if so, display error message
+    function validateField() {
+        if (!query.keywords) {
+            toast.current?.show("Please enter an ingredient!", 3000)
+        } else {
+            startSearch();
+        }
+    }
+
 	return (
 		<View style={styles.screen}>
 			<Header/>
-			<SearchBar onInput={editKeywords} onSubmit={startSearch} keywords={query.keywords}/>
+			<SearchBar onInput={editKeywords} onSubmit={validateField}/>
 			<CuisineFilter selectedCuisine={query.cuisine.toUpperCase()} onSelect={toggleCuisine}/>
-		</View>
+            <Toast 
+                ref={toast} 
+                style={styles.toast} 
+                positionValue={60}
+                fadeInDuration={300}
+                fadeOutDuration={300}/>
+        </View>
 	);
 };
 
 const styles = StyleSheet.create({
 	screen: {
 		paddingTop: Constants.statusBarHeight
-	}
+    },
+    toast: {
+        backgroundColor: Colors.accent
+    }
 })
 
 // mapping functions for redux
 function mapStateToProps(state: any) {
 	return {
+        lastAction: state.lastAction,
 		query: state.query
 	};
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,20 +3,15 @@ import PropTypes from "prop-types";
 import { View, TextInput } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler"
 import SearchIcon from "assets/icons/search.svg";
-import mainStyles from "styles/Main";
+import mainStyles, { Dimens } from "styles/Main";
 import componentStyles from "styles/Header";
-import { Dimens } from "styles/Main";
 
 export default function SearchBar({ onInput, onSubmit } : any) {
 	const { input, buttonIcon } = componentStyles;
 
-	function onChangeHandler(fieldText: string) {
-		onInput(fieldText);
-	}
-
 	return (
 		<View style={ [mainStyles.component, componentStyles.header] }>
-			<TextInput style={input} placeholder="By ingredients" onChangeText={onChangeHandler}/>
+			<TextInput style={input} placeholder="By ingredients" onChangeText={onInput}/>
 			<TouchableOpacity style={buttonIcon} onPress={onSubmit}>
 				<SearchIcon width={Dimens.buttonSizeMain} height={Dimens.buttonSizeMain}/>
 			</TouchableOpacity>
@@ -25,7 +20,6 @@ export default function SearchBar({ onInput, onSubmit } : any) {
 }
 
 SearchBar.propTypes = {
-	keywords: PropTypes.string.isRequired,
 	onInput: PropTypes.func.isRequired,
 	onSubmit: PropTypes.func.isRequired
 }
